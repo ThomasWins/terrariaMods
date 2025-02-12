57b9cc03804a7fb6eb43400fb047ef4397b97b4e
Thomas Winslow <168648705+ThomasWins@users.noreply.github.com>
2025-02-12 17:48:03 -0500

    Update PoopSword.cs

--- Mods/PoopSword/PoopSword.cs
+++ Mods/PoopSword/PoopSword.cs
@@ -7,7 +7,6 @@ using Terraria.ModLoader;
 
 namespace PoopSword
 {
-	// Please read https://github.com/tModLoader/tModLoader/wiki/Basic-tModLoader-Modding-Guide#mod-skeleton-contents for more information about the various files in a mod.
 	public class PoopSword : Mod
 	{
 
